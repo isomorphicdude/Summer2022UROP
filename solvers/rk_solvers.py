a2218309eb3b23865ef1138ab34703dbfc484571
isomorphicdude <47920591+isomorphicdude@users.noreply.github.com>
2022-07-27 21:50:43 +0100

    fix rk solvers

--- solvers/rk_solvers.py
+++ solvers/rk_solvers.py
@@ -14,15 +14,13 @@ def rk2(f, y0, h, dim, times, params):
   - h: float, time-step size
   - dim: int, dimension of the prolem, used to initialize array for storage
   - times: tuple, (start, end)
-  - params: parameters
-  - verbose: bool, indicates whether to print progress (for tracking)  
+  - params: parameters  
 
   Returns:  
   - y: ndarray of values computed at given times
   '''  
   start, end = times  
   t_span = np.arange(start, end, h)
-  N, a, b, c, p = params
 
   # length of time
   t_len = len(t_span)
@@ -32,8 +30,8 @@ def rk2(f, y0, h, dim, times, params):
 
   y[0] = y0  
 
-  for i in tqdm(range(t_len-1)):
-    k1 = np.array(f(y[i], params))
+  for i in tqdm(range(t_len-1)):  
+    k1 = f(y[i], params)
     y1 = y[i] + (h/2) * k1
     k2 = f(y1, params)
     y[i+1] = y[i] + h * k2
@@ -53,10 +51,9 @@ def rk4(f, y0, h, dim, times, params):
   - h: float, time-step size
   - dim: int, dimension of the prolem, used to initialize array for storage
   - times: tuple, (start, end)
-  - params: parameters
-  - verbose: bool, indicates whether to print progress (for tracking)  
+  - params: parameters  
 
-  Returns:
+  Returns:  
   - y: ndarray of values computed at given times
   '''  
   start, end = times  
@@ -71,11 +68,11 @@ def rk4(f, y0, h, dim, times, params):
   y[0] = y0   
   
   for i in tqdm(range(t_len-1)):
-    k1 = np.array(f(y[i], params))
+    k1 = f(y[i], params)
     y1 = y[i] + (h/2) * k1
-    k2 = np.array(f(y1, params))
+    k2 = f(y1, params)
     y2 = y[i] + (h/2) * k2
-    k3 = np.array(f(y2, params))
+    k3 = f(y2, params)
     y[i+1] = y[i] + h * k3
     
   return y
